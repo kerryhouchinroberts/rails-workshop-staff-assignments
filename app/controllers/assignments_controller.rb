1aaefae1c6a5b2785bf8a31f544889a2331670ed
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-26 15:13:33 -0700

    Add ability to edit and delete assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -16,6 +16,32 @@ class AssignmentsController < ApplicationController
     end
   end
 
+  def edit
+    @assignment = Assignment.find(params[:id])
+    @person = Person.find(params[:person_id])
+  end
+
+  def update
+    @assignment = Assignment.find(params[:id])
+    @person = Person.find(params[:person_id])
+    @assignment.person_id = @person.id
+    if @assignment.update_attributes(assignment_params)
+      redirect_to person_path(@person)
+    else
+      flash.now[:alert] = "Please select a location and role."
+      render :new
+    end
+  end
+
+  def destroy
+    @assignment = Assignment.find(params[:id])
+    @person = Person.find(params[:person_id])
+    if @assignment.destroy
+      redirect_to person_path(@person), notice: 'Assignment was destroyed'
+    end
+  end
+
+
   private
   def assignment_params
     params.require(:assignment).permit(:location_id, :role)
